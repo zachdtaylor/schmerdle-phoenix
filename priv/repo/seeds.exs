abfb6d057ada004c04094d3255d15e971de74218
Zach Taylor <zachtylr04@gmail.com>
2022-05-05 00:08:49 -0600

    Add seed script for words

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,37 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+defmodule Seed do
+  @max_pg_parameters 65535
+
+  def load_words_json() do
+    with {:ok, body} <- File.read("./words.json"),
+         {:ok, json} <- Jason.decode(body) do
+      {:ok, json}
+    end
+  end
+
+  def seed_words() do
+    {:ok, data} = load_words_json()
+
+    entries =
+      Enum.map(data, fn word ->
+        %{
+          :value => word,
+          :rating => 0,
+          :inserted_at => NaiveDateTime.local_now(),
+          :updated_at => NaiveDateTime.local_now()
+        }
+      end)
+
+    for chunk <- Enum.chunk_every(entries, div(@max_pg_parameters, 5)) do
+      SchmerdlePhoenix.Repo.insert_all(
+        SchmerdlePhoenix.Word,
+        chunk,
+        on_conflict: :nothing
+      )
+    end
+  end
+end
+
+Seed.seed_words()
