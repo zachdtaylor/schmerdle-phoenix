4a16f7e463cdae86a987ce71ac8d7e552ae09925
Zach Taylor <zachtylr04@gmail.com>
2022-05-29 19:50:38 -0600

    Add apline, extract html to heex, format

--- test/schmerdle_phoenix_web/views/error_view_test.exs
+++ test/schmerdle_phoenix_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule SchmerdlePhoenixWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(SchmerdlePhoenixWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(SchmerdlePhoenixWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
